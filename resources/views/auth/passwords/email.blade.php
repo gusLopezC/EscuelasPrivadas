e8bbb027afeabaf447f77bd94efca0ab11c1802a
gus <gfuslopezcallejas@gmail.com>
2020-04-27 13:46:36 -0500

    Recovery email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,7 +9,7 @@
             <form class="login100-form validate-form" action="{{ route('password.email') }}" method="POST">
                 {{ csrf_field() }}
                 <span class="login100-form-title p-b-43">
-                    Forgot Password?
+                    Olvidaste tu contraseña?
                 </span>
 
 
@@ -21,7 +21,7 @@
 
                 <div class="container-login100-form-btn">
                     <button class="login100-form-btn">
-                        Send Password Reset Link
+                        Enviar link de recuperación
                     </button>
                 </div>
                 <br>
